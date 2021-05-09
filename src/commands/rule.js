0851cf78036f7e2090caea19a8a13741f4117714
verifyLol <66666148+verifyLol@users.noreply.github.com>
2021-05-09 10:44:24 +0300

    Update rule.js

--- src/commands/rule.js
+++ src/commands/rule.js
@@ -4,32 +4,42 @@ module.exports = {
     name: 'rule',
 
     execute(message, args) {
-        const rule1 = ":one: **Innapropriate topics/phrases are not permitted to be used in the server, unless you have been given consent by Golosita Administration, we do not recommend using any as it will result in a punishment.** "
-        const rule2 = ":two: **Profanity is allowed, but do not use it excessively and do not use heavy swearing phrases. This will result in a punishment.**"
-        const rule3 = ":three: **Spamming is not allowed, any sort of excessive spam will result in a punishment.**"
-        const rule4 = ":four: **Any form of harassment or cyberbullying is strictly prohibited in our text channels and voice channels. The Golosita Administration will strictly enforce this rule and shall take any necessary actions of both parties involved in harassment/cyber-bullying cases. This does include any conflict between both parties.**"
-        const rule5 = ":five: **Advertising any content, without the consent of Golosita’s Administration is highly forbidden, and consequences will apply.**"
-        const rule6 = ":six: **NSFW content, such as flashing/pornographic content will not be tolerated. This will result in a punishment.**"
+
+        function returnEmbedRule(text) {
+            const embed = new Discord.MessageEmbed()
+            .setTitle("Server Regulations")
+            .setThumbnail("https://cdn.discordapp.com/icons/834557953264713769/224dea1238ac4e8f40adaf731ce3a49c.png")
+            .setColor("#ffbf00")   
+            .setDescription(text)
+
+            return embed
+        }
 
         if(!args[0]) {
             const errorEbed = new Discord.MessageEmbed()
-            .setDescription('__**Error**__\nArgument is missing!\nAvailable options: 1, 2, 3, 4, 5 6')
+            .setDescription('__**Error**__\nArgument is missing!\nAvailable options:\n`1, 2, 3, 4, 5, 6, 7, 8, tos`')
             .setThumbnail("https://cdn.discordapp.com/icons/834557953264713769/224dea1238ac4e8f40adaf731ce3a49c.png")
             .setColor("#ffbf00")   
             .setTitle(":name_badge: Command Error")
             return message.channel.send(errorEbed)
         } else if(args[0] == "1") {
-            return message.reply(rule1)
+            message.reply(returnEmbedRule("**SR:1** | Profanity is strictly prohibited within our server, any source of profanity will result in consequences depending on your action."))
         } else if(args[0] == "2") {
-            return message.reply(rule2)
+            message.reply(returnEmbedRule("**SR:2** | Any spamming within our server or to a member of our server via Direct Messages will result in consequences depending on your action."))
         } else if(args[0] == "3") {
-            return message.reply(rule3)
+            message.reply(returnEmbedRule("**SR:3** | Any sort of Cyberbullying or Harrasement noticed by a staff member will result in a server ban with a one chance appeal. Other consequences may apply."))
         } else if(args[0] == "4") {
-            return message.reply(rule4)
+            message.reply(returnEmbedRule("**SR:4** | Any kind of racism directed to any member of the community will result in a permanent ban with no appeal, racism is not tolerated here at Golosita."))
         } else if(args[0] == "5") {
-            return message.reply(rule5)
+            message.reply(returnEmbedRule("**SR:5** | Any kind of toxicity directed to any member of the community will result in a 24h mute, and if continued, a 3 day server ban."))
         } else if(args[0] == "6") {
-            return message.reply(rule6)
+            message.channel.send(returnEmbedRule("**SR:6** | Any sort of innapropriate content will result in a permanent ban, this could be pornographic/NSFW, gore, etc."))
+        } else if(args[0] == "7") {
+            message.reply(returnEmbedRule("**SR:7** | Any kind of mocking deaths within our community, especially a server members loss, will result in a permanent ban with no appeal."))
+        } else if(args[0] == "8") {
+            message.reply(returnEmbedRule("**SR:8** | Any sort of advertising in our server without permission from a Golosita administrator will result in a 48h mute."))
+        } else if(args[0] == "tos") {
+            message.reply(returnEmbedRule("__**DISCORD TOS**__\n:link: https://discord.com/guidelines \n:link: https://discord.com/terms"))
         }
     }
 }
