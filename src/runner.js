9f0e86344d1cfe5b11e8be172186297bf6e95aa3
verifyLol <66666148+verifyLol@users.noreply.github.com>
2021-05-10 14:46:03 +0300

    Update runner.js

--- src/runner.js
+++ src/runner.js
@@ -83,7 +83,7 @@ client.on('guildMemberAdd', member => {
     const embed = new Discord.MessageEmbed()
     .setTitle(":wave: | Welcome to Golosita!")
     .setFooter("Enjoy your time at Golosita!")
-    .setDescription(`Hey, ${member}! Welcome to Golosita Discord Server. Consider:\n:orange_book: **Reading rules in <#835794904332107776>,**\n:map: **Joining our group in <#835794788841422860> if you haven't already**`)
+    .setDescription(`Hey, ${member}! Welcome to Golosita Discord Server. Consider:\n:orange_book: **Reading rules and joining our group in <#840696023050616862>**\n:white_sun_rain_cloud: **Got a suggestion about Golosita? Share it in <#840697029256413195>\n:question: **Don't know where to get notified for interviews or trainings? Head to <#840696470184263690>!**`)
     .setThumbnail(member.user.avatarURL({ dynamic: true, size: 4096}))
     .setColor("#ffbf00")
 
