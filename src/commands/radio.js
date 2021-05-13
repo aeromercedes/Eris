125fe5ee65225c3a8d646e7a6e201197254ac059
verifyLol <66666148+verifyLol@users.noreply.github.com>
2021-05-13 21:35:54 +0300

    Update radio.js

--- src/commands/radio.js
+++ src/commands/radio.js
@@ -16,7 +16,7 @@ module.exports = {
             return message.channel.send(novc)
         } else if (!args[0]) {
             const noArgs = new Discord.MessageEmbed()
-            .setDescription('__**Error**__\nThere are no arguments!\nAvailable arguments:\npal, metro, fenomen, power')
+            .setDescription('__**Error**__\nThere are no arguments!\nAvailable arguments:\npal, metro, fenomen, power, niceguys')
             .setTitle(":name_badge: Command Error")
             .setThumbnail("https://cdn.discordapp.com/icons/834557953264713769/224dea1238ac4e8f40adaf731ce3a49c.png")
             .setColor("#ffbf00")
@@ -82,6 +82,22 @@ module.exports = {
                 .setThumbnail("https://cdn.discordapp.com/icons/834557953264713769/224dea1238ac4e8f40adaf731ce3a49c.png")
                 .setColor("#ffbf00")
                 message.reply(embed)
+            } else if (args[0].toLowerCase() == "niceguys") {
+                broadcast = client.voice.createBroadcast()
+
+                const connection = await message.member.voice.channel.join()
+                connection.play(broadcast)           
+                broadcast.play(await ytdl("https://www.youtube.com/watch?v=oVi5gtzTDx0"))
+                log("info", "Loading Nice Guys FM")   
+                
+                const embed = new Discord.MessageEmbed()
+                .setTitle(":musical_note: Radio")
+                .setDescription(":white_check_mark: Radio loaded: **Nice Guys FM**\n")
+                .setFooter('thanks hailee <3')
+                .setThumbnail("https://cdn.discordapp.com/icons/834557953264713769/224dea1238ac4e8f40adaf731ce3a49c.png")
+                .setColor("#ffbf00")
+                message.reply(embed)
+
             }
         }
     }
