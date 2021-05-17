95748fe0986377f1353d4eba4448d81a580930ae
verifyLol <66666148+verifyLol@users.noreply.github.com>
2021-05-18 00:05:40 +0300

    Add files via upload

---
+++ src/commands/fplink.js
@@ -0,0 +1,37 @@
+module.exports = {
+    name: "fplink",
+    administratorOnly = true,
+
+    execute(message, args) {
+        if(!message.member.voice.channel) {
+            const novc = new Discord.MessageEmbed()
+            .setDescription('__**Error**__\nYou are not in a voice channel!')
+            .setTitle(":name_badge: Command Error")
+            .setThumbnail("https://cdn.discordapp.com/icons/834557953264713769/224dea1238ac4e8f40adaf731ce3a49c.png")
+            .setColor("#ffbf00")
+            return message.channel.send(novc)
+        } else if (!args[0]) {
+            const noArgs = new Discord.MessageEmbed()
+            .setDescription('__**Error**__\nThere are no arguments!\nAvailable arguments:\npal, metro, fenomen, power, niceguys')
+            .setTitle(":name_badge: Command Error")
+            .setThumbnail("https://cdn.discordapp.com/icons/834557953264713769/224dea1238ac4e8f40adaf731ce3a49c.png")
+            .setColor("#ffbf00")
+            return message.channel.send(noArgs)
+        }
+        
+        broadcast = client.voice.createBroadcast()
+
+        const connection = await message.member.voice.channel.join()
+        connection.play(broadcast)           
+        broadcast.play(await ytdl(args[0]))
+        log("info", "playing fplik")   
+        
+        const embed = new Discord.MessageEmbed()
+        .setTitle(":musical_note: Radio")
+        .setDescription(":white_check_mark: done have fun.")
+        .setFooter('...')
+        .setThumbnail("https://cdn.discordapp.com/icons/834557953264713769/224dea1238ac4e8f40adaf731ce3a49c.png")
+        .setColor("#ffbf00")
+        message.reply(embed)
+    }
+}
